d199633f0cb333d0ef7ecfc9ec8b1a7ee45ea20d
BenCmjn <benjamin.calmejane@gmail.com>
2018-02-05 16:07:12 +0100

    nouveau controller session + bootstrap installed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,19 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(params[:user])    # Not the final implementation!
     if @user.save
-      redirect_to @user
+      # Handle a successful save.
     else
       render 'new'
     end
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-  end
-
-  private
-  def gossip_params
-    params.require(:gossip).permit(:anonymous_author, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'static_pages/home'
+
+  get 'static_pages/secret'
+
   get 'users/new'
 
   get 'users/create'
@@ -7,5 +17,6 @@ Rails.application.routes.draw do
 
   get 'users/destroy'
 
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
